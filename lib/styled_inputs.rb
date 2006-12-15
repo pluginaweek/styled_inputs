e062e9a8db1be1e5a8f3bbd22e0748043d83c0d1
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2006-12-15 20:20:27 +0000

    Added tests and documentation.

--- lib/styled_inputs.rb
+++ lib/styled_inputs.rb
@@ -2,9 +2,29 @@ require 'set_or_append'
 
 module PluginAWeek #:nodoc:
   module Helpers #:nodoc:
+    # Automatically adds css classes to input tags so that fields can be
+    # easily styled.
+    # 
+    # For example, using tags:
+    #   
+    #   text_field_tag('name')        # => <input class="text" id="name" name="name" type="text" />
+    #   hidden_field_tag('name')      # => <input class="hidden" id="name" name="name" type="hidden" />
+    #   radio_button_tag('agree', 1)  # => <input class="radio" id="agree_1" name="agree" type="radio" value="1" />
+    # 
+    # Using the form helpers:
+    # 
+    #   text_field(:person, :name)        # => <input class="text" id="person_name" name="person[name]" size="30" type="text" />
+    #   hidden_field(:person, :name)      # => <input class="hidden" id="person_name" name="person[name]" type="hidden" />
+    #   radio_button(:person, :agree, 1)  # => <input class="radio" id="person_agree_1" name="person[agree]" type="radio" value="1" />
+    # 
+    # If you specify additional classes when creating a tag, the automated css
+    # classes will be appended to the current ones.  For example,
+    # 
+    #   text_field_tag('name', :class => 'selected')        # => <input class="selected text" id="name" name="name" type="text" />
+    #   text_field_tag('name', :class => 'selected shadow') # => <input class="selected shadow text" id="name" name="name" type="text" />
     module StyledInputHelper
-      #
-      #
+      # Appends the input type to the value currently stored in the html options
+      # for the tag.
       def styled_input(name, options)
         options = options.stringify_keys
         
